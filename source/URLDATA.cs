9e8c060acc54df247d275c4494f681fbb63e7dd3
stockingsloverr <cavabanga027@gmail.com>
2026-01-10 16:27:07 +0100

    Create URLDATA.cs

---
+++ source/URLDATA.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalisAPI
+{
+    internal class URLDATA
+    {
+        public class DownloadUrlData
+        {
+            public string L1 { get; set; }
+
+            public string L2 { get; set; }
+
+            public string question { get; set; }
+        }
+    }
+}
